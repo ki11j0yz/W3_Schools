4e095422e507505d3b20ad2aed1f5e996212e056
ki11j0yz <116903454+ki11j0yz@users.noreply.github.com>
2023-06-15 22:11:02 -0500

    4th commit

---
+++ Assignment/assignment.py
@@ -0,0 +1,19 @@
+# assignment operators are used to assign values to variables
+
+obj = """
+  '=': {'x = 5': 'same as x = 5',}, 
+  '+=': {'x += 3': 'same as x = x + 3'}, 
+  '-=': {'x -= 3': 'same as x = x - 3'},
+  '*=': {'x *=': 'same as x = x * 3'},
+  '/=': {'x /= 3': 'same as x = x / 3'}, 
+  '%=': {'x %= 3': 'same as x = x % 3'},
+  '//=': {'x //= 3': 'same as x = x //3'}, 
+  '**=': {'x **= 3': 'same as x = x ** 3'}, 
+  '&=': {'x &= 3': 'same as x = x & 3'}, 
+  '|=': {'x |= 3': 'same as x = x | 3'}, 
+  '^=': {'x ^= 3': 'same as x = x ^ 3'}, 
+  '>>=': {'x >>= 3': 'same as x = x >> 3'}, 
+  '<<=': {'x <<= 3': 'sasme as x = x << 3'}, 
+"""
+
+print(obj)
