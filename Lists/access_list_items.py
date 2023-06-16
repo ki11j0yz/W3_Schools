fdc71bb6499e449f9ad4b1957ba2cbfc86004ace
ki11j0yz <116903454+ki11j0yz@users.noreply.github.com>
2023-06-15 22:22:16 -0500

    Delete Lists directory

--- Lists/access_list_items.py
+++
@@ -1,6 +0,0 @@
-# lists are used to store multiple items in a single variable and are created using '[]'
-# lists are one of 4 data types in python, the other three are: 
-  # Tuple, Set, and Dictionary
-
-obj = ["apple", "banana", "cherry"]
-print(obj)
