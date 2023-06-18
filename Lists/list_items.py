8edba0e9ddbe1a2d2f4d45be08ff88b646826660
ki11j0yz <116903454+ki11j0yz@users.noreply.github.com>
2023-06-18 10:33:45 -0500

    lists commit

---
+++ Lists/list_items.py
@@ -0,0 +1,5 @@
+# list items are ordered, changeable, and allow duplicate values. 
+# list items are indexed, the first item has index [0], the second item has index [1], etc. 
+  # ordered means that the items have a defined order and it will not change. 
+    # new items added are placed at the end of the list. 
+
