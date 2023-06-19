052bbd2dfb092bbc9fcb1bc6780534a88f6daab3
ki11j0yz <116903454+ki11j0yz@users.noreply.github.com>
2023-06-19 01:35:18 -0500

    added to Lists notes, restructured/organized Lists

---
+++ Lists/Change List Items/Removing List Items/clear.py
@@ -0,0 +1,4 @@
+# the clear() method empties the list - but the list still remains, but without content
+mylist = ["apple", "banana", "cherry"]
+mylist.clear()
+print(mylist)   # will output '[]' to signify an empty list
