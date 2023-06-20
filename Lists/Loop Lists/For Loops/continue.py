44a199cdf21060bb6a0ce53a145645b1e8f1f959
James <jkprice02@gmail.com>
2023-06-19 20:50:47 -0500

    initial commit from laptop

--- Lists/Loop Lists/For Loops/continue.py
+++ Lists/Loop Lists/For Loops/continue.py
@@ -3,4 +3,13 @@ veggies = ["broccoli", "carrots", "asparagus", "peppers", "corn", "green beans"]
 for x in veggies: 
     if x == "peppers":
         continue
-    print(x)
+    print(x)      # it skipped 'peppers' and continued on to the end of the list - you stopped the 'peppers' iteration
+print("\n")
+
+fruits = ["apples", "banana", "pear", "watermelon", "papaya"]
+for y in fruits:
+    if y == "watermelon":
+        print(y)
+    continue    # if the 'continue' and print statement were reversed, the output would be the same as the code above
+
+    
--- Lists/Loop Lists/For Loops/range.py
+++ Lists/Loop Lists/For Loops/range.py
@@ -2,3 +2,6 @@
 # returns a sequence of numbers, starting from 0 by default, and increments 1 (by default), and ends at a specified number
 for x in range(10):
     print(x)
+# note that range(10), the sequence stops at 9, not 10 - if you were to change to range(11) - you would print out 10.
+
+
