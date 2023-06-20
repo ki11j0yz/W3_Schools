a9e843be6f7b16fa675c625c112a4c5cdf44b20a
ki11j0yz <116903454+ki11j0yz@users.noreply.github.com>
2023-06-19 20:08:19 -0500

    updated notes in Python/Loop Lists/For Loops

---
+++ Lists/Loop Lists/For Loops/range.py
@@ -0,0 +1,4 @@
+# the range() function allows looping through code a specified number of times
+# returns a sequence of numbers, starting from 0 by default, and increments 1 (by default), and ends at a specified number
+for x in range(10):
+    print(x)
